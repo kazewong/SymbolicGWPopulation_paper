979a6eff78a5721a04b56b2cef738b8f6783050f
Kaze Wong <kazewong.physics@gmail.com>
2022-07-20 17:55:47 -0400

    Update plotting scripts

--- src/scripts/figure1_subparts.py
+++ src/scripts/figure1_subparts.py
@@ -5,9 +5,29 @@ import sympy
 import pandas as pd
 from sympy import preorder_traversal, Float
 
+
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = 'Computer Modern Raman'
 plt.rcParams['text.usetex'] = True
+
+GWTC = np.load('./src/data/GWTC3_posterior.npz')
+m1 = GWTC['m1']
+from scipy.stats import gaussian_kde
+m1_axis = np.linspace(0,100,1000)
+fig, ax = plt.subplots(1,1,figsize=(10,9))
+for i in range(79):
+    kde = gaussian_kde(m1[i,::10])
+    ax.plot(m1_axis,kde(m1_axis),c='grey',alpha=0.5)
+ax.set_yscale('log')
+ax.set_ylim(1e-3,3)
+ax.set_xlim(0,100)
+ax.set_xlabel(r'$m_{1,i}\ [M_{\odot}]$',fontsize=20)
+ax.set_ylabel(r'$p(m_{1,i})$',fontsize=20)
+ax.tick_params(axis='x',labelsize=16)
+ax.tick_params(axis='y',labelsize=16)
+ax.set_title('Data',fontsize=24)
+fig.savefig('./figure/GWTC3_m1_data.pdf',bbox_inches='tight')
+
 
 def get_data():
     data = np.load("GWTC3_m1_spectrum.npz")
@@ -118,13 +138,24 @@ label3 = label3.replace('&','\&')
 
 
 
-fig, ax = plt.subplots(1,1,figsize=(20,9))
-ax.plot(X['M'].values[:-2],np.exp(X['logP'])[:-2],label='Gaussian Mixture Data',lw=5)
+fig, ax = plt.subplots(1,1,figsize=(10,9))
+ax.plot(X['M'].values[:-2],np.exp(X['logP'])[:-2],lw=5)
 ax.fill_between(X['M'].values[:-2],np.exp(X['logP_low'][:-2]),np.exp(X['logP_high'][:-2]),alpha=0.14,color='C0')
-ax.plot(X['M'].values[:-2],np.abs(y1[:-2]),label='PySR,'+label1,lw=3)
-ax.plot(X['M'].values[:-2],np.abs(y2[:-2]),label='PySR,'+label2,lw=3)
-ax.plot(X['M'].values[:-2],np.abs(y3[:-2]),label='PySR,'+label3,lw=3)
-#plt.plot(X['M'].values[:-2],y[:-2]-np.exp(X['logP'])[:-2])
+ax.set_yscale('log')
+ax.set_xlabel(r'$m_1\ [M_{\odot}]$',fontsize=20)
+ax.set_ylabel(r'$d\mathcal{R}/dm_1\ [{\mathrm yr}^{-1}]$',fontsize=20)
+ax.tick_params(axis='x',labelsize=16)
+ax.tick_params(axis='y',labelsize=16)
+ax.set_ylim(1e-6,2e1)
+ax.set_xlim(0,100)
+ax.set_title('Gaussian Mixture Model',fontsize=24)
+fig.savefig('./figure/GWTC3_m1_GMM.pdf',bbox_inches='tight')
+
+
+fig, ax = plt.subplots(1,1,figsize=(10,9))
+ax.plot(X['M'].values[:-2],np.abs(y1[:-2]),label='Low complexity',lw=3)
+ax.plot(X['M'].values[:-2],np.abs(y2[:-2]),label='Mid complexity',lw=3)
+ax.plot(X['M'].values[:-2],np.abs(y3[:-2]),label='High complexity',lw=3)
 ax.set_yscale('log')
 ax.legend(loc='lower right',fontsize=16)
 ax.set_xlabel(r'$m_1\ [M_{\odot}]$',fontsize=20)
@@ -133,6 +164,5 @@ ax.tick_params(axis='x',labelsize=16)
 ax.tick_params(axis='y',labelsize=16)
 ax.set_ylim(1e-6,2e1)
 ax.set_xlim(0,100)
-#plt.title(title,fontsize=20)
-
-plt.savefig("./GWTC3_Gaussian_m1_fit.pdf",bbox_inches='tight')
+ax.set_title('Symbolic Forms',fontsize=24)
+fig.savefig('./figure/GWTC3_m1_PYSR.pdf',bbox_inches='tight')
